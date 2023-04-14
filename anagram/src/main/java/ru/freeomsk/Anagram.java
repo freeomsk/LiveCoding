376e243c9983678b1e73db09b865c8d1a0d1aab2
freeomsk <freeomsk@yandex.ru>
2023-04-14 11:50:52 +0600

    Add new variant for anagram

--- anagram/src/main/java/ru/freeomsk/Anagram.java
+++ anagram/src/main/java/ru/freeomsk/Anagram.java
@@ -4,18 +4,22 @@ import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class Anagram {
     public static void main(String[] args) {
 
-        String s1 = "cat";
-        String s2 = "tac";
+        String s1 = "qwerty";
+        String s2 = "eqyrwt";
 
         System.out.println(isAnagramSort(s1, s2));
         System.out.println(isAnagramSortMinimum(s1, s2));
         System.out.println(isAnagramCounting(s1, s2));
         System.out.println(isAnagramMultiset(s1, s2));
         System.out.println(isLetterBasedAnagramMultiset(s1, s2));
+        System.out.println(checkAnagram(s1, s2));
     }
 
     //     1 вариант - проверка с помощью сортировки
@@ -71,7 +75,24 @@ public class Anagram {
         return multiset1.equals(multiset2);
     }
 
-// 5 вариант - анаграмма на основе букв.
+    // 5 вариант - через Map
+
+    private static boolean checkAnagram(String s1, String s2) {
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        for (Character c : s1.toCharArray()){
+            map1.merge(c, 1, Integer::sum);
+        }
+        for (Character c : s2.toCharArray()){
+            map2.merge(c, 1, Integer::sum);
+        }
+        for(Character c : map1.keySet()) {
+            if (!Objects.equals(map1.get(c), map2.get(c)))
+                return false;
+        }
+        return s1.length() == s2.length();
+    }
+// 6 вариант - анаграмма на основе букв.
 // Приведенные до сих пор примеры не соответствуют строго лингвистическому определению анаграммы.
 // Это потому, что они считают знаки препинания частью анаграммы, и они чувствительны к регистру.
 // Адаптируем алгоритмы, чтобы включить анаграмму на основе букв.
