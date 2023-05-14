e76cbf1e340efdd9b9fb7ee9ffbacc11869e0b7c
freeomsk <freeomsk@yandex.ru>
2023-05-14 10:08:07 +0600

    Add module countShips

---
+++ countShips/src/main/java/ru/freeomsk/CountShips.java
@@ -0,0 +1,33 @@
+package ru.freeomsk;
+
+public class CountShips {
+    public static void main(String[] args) {
+        int[][] board = {
+                {1, 1, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1, 0},
+                {1, 0, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0},
+                {1, 0, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0, 0}
+        };
+        System.out.println(countShips(board));
+    }
+
+    public static int countShips(int[][] board) {
+        int count = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 1) {
+                    if (i > 0 && board[i - 1][j] == 1) {
+                        continue;
+                    }
+                    if (j > 0 && board[i][j - 1] == 1) {
+                        continue;
+                    }
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
