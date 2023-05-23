982f2c03573176c344b88f82069c80690f5bcc84
freeomsk <freeomsk@yandex.ru>
2023-05-23 22:20:18 +0600

    Update module countUniqueElementsArray

--- countUniqueElementsArray/src/main/java/ru/freeomsk/CountUniqueElementsArrayApplication.java
+++ countUniqueElementsArray/src/main/java/ru/freeomsk/CountUniqueElementsArrayApplication.java
@@ -10,8 +10,12 @@ public class CountUniqueElementsArrayApplication {
         int[] arr = new int[] { 9, 0, 8, 0, 1, 2, 4, 4, 5, 7, 8, 4, 3, 9, 2, 1, 7, 4, 6, 4, 1, 2};
 
         CountUniqueElementsArray(arr);
+        System.out.println("______________________");
         CountUniqueElementsArrayWithoutFilter(arr);
+        System.out.println("______________________");
         CountUniqueElementsArrayWithFilter(arr);
+        System.out.println("______________________");
+        CountUniqueElementsArray2(arr);
     }
     // 1 вариант (вывод в 2 колонки с табуляцией)
     public static void CountUniqueElementsArray(int[] arr) {
@@ -40,5 +44,21 @@ public class CountUniqueElementsArrayApplication {
         Arrays.stream(arr).forEach(x -> map.merge(x, 1, Integer::sum));
         map.entrySet().stream().filter(x -> x.getValue() > 0)
                 .forEach(x -> System.out.println(x.getKey() + "=" + x.getValue()));
+    }
+
+    // 4 вариант - ChatGPT-4
+    // Создаем HashMap для хранения элементов и их количества
+    public static void CountUniqueElementsArray2(int[] arr) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        // Проходим по массиву и увеличиваем счетчик для каждого элемента
+        for (int element : arr) {
+            countMap.put(element, countMap.getOrDefault(element, 0) + 1);
+        }
+
+        // Выводим результат
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            System.out.println("Элемент " + entry.getKey() + " повторяется " + entry.getValue() + " раз(а)");
+        }
     }
 }
