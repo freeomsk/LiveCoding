a3df641455154a946f8d20dee7f6c0b8f9911614
freeomsk <freeomsk@yandex.ru>
2023-05-25 14:51:37 +0600

    Update module evenIterator

--- evenIterator/src/main/java/ru/freeomsk/EvenIterator.java
+++ evenIterator/src/main/java/ru/freeomsk/EvenIterator.java
@@ -7,9 +7,10 @@ public class EvenIterator implements Iterator<Integer> {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         EvenIterator evenIterator = new EvenIterator(numbers);
-        while (evenIterator.hasNext()) {
-            System.out.println(evenIterator.next());
-        }
+//        while (evenIterator.hasNext()) {
+//            System.out.println(evenIterator.next());
+//        }
+        evenIterator.forEachRemaining(System.out::println);
     }
 
     private final Iterator<Integer> iterator;
